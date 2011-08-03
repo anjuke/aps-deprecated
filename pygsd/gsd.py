b6175bef94746ee5e75c7877a70c7c58783be442
Zhang Erning <erning@anjuke.com>
2011-08-03 18:06:51 +0800

    refactor, still under construction

--- pygsd/gsd.py
+++ pygsd/gsd.py
@@ -7,7 +7,6 @@ import subprocess
 import time
 import msgpack
 import zmq
-from zmq.eventloop import ioloop, zmqstream
 
 VERSION = r'APS10'
 EMPTY = r''
@@ -18,25 +17,59 @@ def microtime():
 def millitime():
     return int(round(time.time() * 1000))
 
-class GSD:
-    feps = []       # frontend endpoints
-    beps = []       # backend endpoints
-    meps = []       # monitor endpoints
-    minw = 1        # min worker
-    maxw = 32       # max worker
-    spaw = 8        # spare worker
-    timeout = 10000 # worker timeout
-    interval = 1000 # maintain interval
-    args = []
-
-    def start(self):
-        for i in xrange(self.minw):
-            self.create_worker()
-
-        self.spare_workers = []
+class Workers:
+    def __init__(self):
         self.workers = {}
+        self.queue = []
+        self.leased = {}
 
-        self.pending_requests = []  # to hold requests if there's no worker available
+    def is_available(self):
+        while self.queue:
+            wid = self.queue[0]
+            if wid in self.workers:
+                return True
+            self.queue.pop(0)
+        return False
+
+    def add(self, wid):
+        if wid not in self.workers:
+            self.queue.append(wid)
+        elif wid in self.leased:
+            del self.leased[wid]
+            self.queue.append(wid)
+        self.workers[wid] = millitime()
+
+    def remove(self, wid):
+        if wid in self.leased:
+            del self.leased[wid]
+        if wid in self.workers:
+            del self.workers[wid]
+
+    def borrow(self):
+        while self.queue:
+            wid = self.queue.pop(0)
+            if wid in self.workers:
+                self.leased[wid] = millitime()
+                return wid
+        return None
+
+    def order_than(self, timestamp):
+        workers = []
+        for wid in self.queue:
+            try:
+                if self.worker[wid] > timestamp:
+                    break
+            except KeyError:
+                pass
+            else:
+                workers.append(wid)
+        return workers 
+
+
+class Device():
+    def __init__(self, options):
+        self.options = options
+        self.workers = Workers()
 
         def create_socket(socktype, endpoints):
             socket = zmq.Socket(zmq.Context.instance(), socktype)
@@ -45,89 +78,97 @@ class GSD:
                 socket.bind(endpoint)
             return socket
 
-        if self.feps:
-            feps = self.feps
-        else:
-            feps = ['tcp://*:5000']
-        if self.beps:
-            beps = self.beps
-        else:
-            beps = ['tcp://*:5001']
-        if self.meps:
-            meps = self.meps
-        else:
-            meps = ['tcp://*:5002']
+        self.client_socket = create_socket(zmq.XREP, self.options.feps)
+        self.worker_socket = create_socket(zmq.XREP, self.options.beps)
+        self.monitor_socket = create_socket(zmq.PUB, self.options.meps)
 
-        self.client_socket = create_socket(zmq.XREP, feps)
-        self.worker_socket = create_socket(zmq.XREP, beps)
-        self.monitor_socket = create_socket(zmq.PUB, meps)
+        self.interrupted = False
 
-        self.loop = ioloop.IOLoop.instance()
-        self.client_stream = zmqstream.ZMQStream(self.client_socket, self.loop)
-        self.worker_stream = zmqstream.ZMQStream(self.worker_socket, self.loop)
-        self.monitor_stream = zmqstream.ZMQStream(self.monitor_socket, self.loop)
-
-        self.client_stream.on_recv(self.handle_client)
-        self.worker_stream.on_recv(self.handle_worker)
-
-        self.maintainer = ioloop.PeriodicCallback(self.maintain, self.interval, self.loop)
-
-        self.maintainer.start()
-        self.loop.start()
+    def start(self):
+        self.loop()
 
     def stop(self):
-        self.loop.stop()
+        pass
+    
+    def loop(self):
+        poller = zmq.Poller()
+        poller.register(self.worker_socket, zmq.POLLIN)
 
-    def maintain(self):
-        self.maintain_workers()
+        while not self.interrupted:
+            if self.workers.is_available():
+                poller.register(self.client_socket, zmq.POLLIN)
+            else:
+                poller.register(self.client_socket, 0)
 
-    def handle_client(self, frames):
-        i = frames.index(EMPTY)
-        if frames[i+1] != VERSION:
-            pass # handle version mismatch
-        sequence, timestamp, expiry = msgpack.unpackb(frames[i+2])
-        # handle expired
+            try:
+                events = poller.poll(self.options.interval)
+            except zmq.ZMQError as e:
+                if e.errno == 4:    # system interrupted
+                    continue
+                else:
+                    pass
 
-        wid = self.borrow_worker()
-        if wid:
+            for socket, flag in events:
+                if socket == self.worker_socket:
+                    self.handle_worker()
+                elif socket == self.client_socket:
+                    self.handle_client()
+                else:
+                    assert False
+
+    def handle_client(self):
+        while self.workers.is_available():
+            try:
+                frames = self.client_socket.recv_multipart(zmq.NOBLOCK)
+            except zmq.ZMQError as e:
+                if e.errno == 35:   # no more message to handle
+                    break
+                else:
+                    pass
+
+            i = frames.index(EMPTY)
+            if frames[i+1] != VERSION:
+                pass # handle version mismatch
+            sequence, timestamp, expiry = msgpack.unpackb(frames[i+2])
+            # handle expired
+
+            wid = self.workers.borrow()
+            assert wid
             frames = self.build_worker_request(wid, frames[:i], frames[i+2:])
-            self.worker_stream.send_multipart(frames)
-        else:
-            self.pending_requests.append(frames)
+            worker_socket.send_multipart(frames)
 
-        return wid
+    def hanele_worker(self):
+        while True:
+            try:
+                frames = self.worker_socket.recv_multipart(zmq.NOBLOCK)
+            except zmq.ZMQError as e:
+                if e.errno == 35:   # no more message to handle
+                    break
+                else:
+                    pass
 
-    def handle_pending_requests(self):
-        while self.pending_requests:
-            frames = self.pending_requests.pop(0)
-            if not self.handle_client(frames):
-                break
+            i = frames.index(EMPTY)
+            assert(i == 1)
+            wid = frames[0]
 
-    def handle_worker(self, frames):
-        i = frames.index(EMPTY)
-        assert(i == 1)
-        wid = frames[0]
+            if frames[i+1] != VERSION:
+                pass # handle version mismatch
 
-        if frames[i+1] != VERSION:
-            pass # handle version mismatch
+            command = frames[i+2]
+            if command == '\x00':   # REQUEST
+                j = frames.index(EMPTY, i+3)
+                frames = self.build_client_reply(frames[i+3:j], frames[j+1:])
+                self.client_socket.send_multipart(frames)
+                self.workers.add(wid)
 
-        command = frames[i+2]
-        if command == '\x00':   # REQUEST
-            j = frames.index(EMPTY, i+3)
-            frames = self.build_client_reply(frames[i+3:j], frames[j+1:])
-            self.client_stream.send_multipart(frames)
-            self.return_worker(wid)
-            self.handle_pending_requests()
+            elif command == '\x01': # HEARTBEAT
+                self.workers.add(wid)
 
-        elif command == '\x01': # HEARTBEAT
-            self.return_worker(wid)
-            self.handle_pending_requests()
+            elif command == '\x02': # GOODBYE
+                pass #
 
-        elif command == '\x02': # GOODBYE
-            pass #
-
-        else:
-            pass # handle unknown command
+            else:
+                pass # handle unknown command   
 
     def build_worker_request(self, wid, envelope, body):
         frames = [wid, EMPTY, VERSION, '\x00']
@@ -143,60 +184,9 @@ class GSD:
         frames.extend(body)
         return frames
 
-    def create_worker(self):
-        p = subprocess.Popen(self.args)
-        print p.pid
 
-    def borrow_worker(self):
-        if not self.spare_workers:
-            return None
-
-        wid = self.spare_workers.pop(0)
-        (timestamp, leased) = self.workers[wid]
-        self.workers[wid] = (timestamp, True)
-        return wid
-
-    def return_worker(self, wid):
-        try:
-            (timestamp, leased) = self.workers[wid]
-        except KeyError:
-            self.spare_workers.append(wid)
-        else:
-            if leased:
-                self.spare_workers.append(wid)
-
-        self.workers[wid] = (millitime(), False)
-
-    def maintain_workers(self):
-        return
-        """ 1. terminate dead workers """
-        """ 2. create new worker if too few """
-        """ 3. terminate spare workers if too many """
-        now = millitime()
-        expiry = now - self.timeout
-        size = len(self.spare_workers)
-        i = 0
-        while i < size:
-            wid, timestamp = self.spare_workers[i]
-            if timestamp >= expiry: 
-                break
-            i += 1
-        expired_spare_workers = self.spare_workers[:i]
-        expired_leased_workers = list(wid
-            for wid, timestamp in self.leased_workers.iteritems() if timestamp < expiry)
-
-        self.spare_workers = self.spare_workers[i+1:]
-        for wid in expired_leased_workers:
-            del self.leased_workers[wid]
-
-        # say GOODBYE to workers?
-        # kill the process?
-
-
-
-def usage():
-    print('usage: %s [options] <worker command line>' % os.path.basename(sys.argv[0]))
-    print """
+class Options:
+    """
 options:
     -h, --help            
         Show this help message and exit
@@ -207,7 +197,7 @@ options:
 
     -b <endpoint>, --backend=<endpoint>
         The binding endpoints where workers will connect to
-        [default: ipc://tmp/(pid-of-gsd).ipc]
+        [default: ipc://*:5001]
 
     -m <endpoint>, --monitor=<endpoint>
         The binding endpoints where monitor events will be published to
@@ -223,7 +213,7 @@ options:
         The maxinum number of spare workers [default: 8]
 
     --timeout=<milliseconds>
-        Worker timeout in millisecond [default: 1000]
+        Worker timeout in millisecond [default: 10000]
     
     --interval=<milliseconds>
         Maintain interval in millisecond [default: 1000]
@@ -231,56 +221,85 @@ options:
     -d, --daemon
 
     -v, --verbose
-"""
-    sys.exit(2)
+    """
 
-def main():
-    print "Generic Service Daemon"
-    print
-    gsd = GSD()
+    def __init__(self, argv):
+        self.prog = os.path.basename(argv[0])
 
-    # parse args
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hf:b:m:n:x:s:i:dv', ['help',
+        self.feps = []       # frontend endpoints
+        self.beps = []       # backend endpoints
+        self.meps = []       # monitor endpoints
+        self.minw = 1        # min worker
+        self.maxw = 32       # max worker
+        self.spaw = 8        # spare worker
+        self.timeout = 10000 # worker timeout
+        self.interval = 1000 # maintain interval
+        self.args = []
+
+        self.errno = 0
+         
+        try:
+            self.parse(argv)
+        except:
+            self.errno = 2
+            pass
+
+    def parse(self, argv):
+        opts, args = getopt.getopt(argv[1:], 'hf:b:m:n:x:s:i:dv', ['help',
             'frontend=', 'backend=', 'monitor=',
             'min-worker=', 'max-worker=', 'spare-worker=',
             'timeout=', 'interval=', 'daemon=', 'verbose='])
-    except getopt.GetoptError, err:
-        usage()
 
-    for o, a in opts:
-        if o in ('-h', '--help'):
-            usage()
-        elif o in ('-f', '--frontend'):
-            gsd.feps.append(a)
-        elif o in ('-b', '--backend'):
-            gsd.beps.append(a)
-        elif o in ('-m', '--monitor'):
-            gsd.meps.append(a)
-        elif o in ('-n', '--min-worker'):
-            gsd.minw = int(a)
-        elif o in ('-x', '--max-worker'):
-            gsd.maxw = int(a)
-        elif o in ('-s', '--spare-worker'):
-            gsd.spaw = int(a)
-        elif o in ('--timeout='):
-            gsd.timeout = int(a)
-        elif o in ('--interval='):
-            gsd.interval = int(a)
-        elif o in ('-d', '--daemon'):
-            pass
-        elif o in ('-v', '--verbose'):
-            verbose = True
-        else:
-            usage()
+        for o, a in opts:
+            if o in ('-h', '--help'):
+                self.usage()
+                self.errno = 1
+                return
+            elif o in ('-f', '--frontend'):
+                self.feps.append(a)
+            elif o in ('-b', '--backend'):
+                self.beps.append(a)
+            elif o in ('-m', '--monitor'):
+                self.meps.append(a)
+            elif o in ('-n', '--min-worker'):
+                self.minw = int(a)
+            elif o in ('-x', '--max-worker'):
+                self.maxw = int(a)
+            elif o in ('-s', '--spare-worker'):
+                self.spaw = int(a)
+            elif o in ('--timeout='):
+                self.timeout = int(a)
+            elif o in ('--interval='):
+                self.interval = int(a)
+            elif o in ('-d', '--daemon'):
+                self.daemon = True
+            elif o in ('-v', '--verbose'):
+                self.verbose = True
+            else:
+                pass
 
-    if (not args):
-        usage()
+        if (not args):
+            self.errno = 3
 
-    gsd.args = args
+        if not self.feps:
+            self.feps = ['tcp://*:5000']
+        if not self.beps:
+            self.beps = ['tcp://*:5001']
 
-    gsd.start()
-    return 0
+    def usage(self):
+        print('usage: %s [options] <worker command line>' % self.prog)
+        print self.__doc__
+        sys.exit(1)
+
+
+def main():
+    options = Options(sys.argv)
+    if options.errno:
+        options.usage()
+        return options.errno
+
+    device = Device(options)
+    device.start()
 
 
 if __name__ == '__main__':
